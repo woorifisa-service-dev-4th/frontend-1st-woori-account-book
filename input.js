40b5d73f538452a989e8210a5e784a3427922faf
yezanee <rbfrk02@naver.com>
2025-01-09 12:34:34 +0900

    Feat: input 타입, 글자수 제한, option 선택안되어있을때는 추가불가, 추가한 후 추가창 띄우기

--- input.js
+++ input.js
@@ -14,6 +14,12 @@ console.log(dateValue);
 /* 입력 값 저장 */
 dateInput.addEventListener('input', (event) => {
     dateValue = event.target.value;
+
+    if(!dateValue) {
+        alert("날짜를 입력해주세요.")
+        dateInput.focus();
+        return;
+    }
     console.log(dateValue);
 })
 
@@ -24,21 +30,21 @@ selectOption.addEventListener('change', (event) => {
 
 descriptionInput.addEventListener('input', (event) => {
 
-    descriptionValue = event.target.value;
+    descriptionValue = event.target.value.trim();
     console.log(descriptionValue);
 
 })
 
 moneyInput.addEventListener('input', (event) => {
 
-    moneyValue = event.target.value;
+    moneyValue = event.target.value.trim();
     console.log(moneyValue);
 
 })
 
 
-/* 추가 버튼 누를 시 입력 값들 TABLE에 전달 */
-addButtonInput.addEventListener('click', async() => {
+/* 추가 버튼 누를 시 필요한 입력 값들 TABLE에 전달 */
+addButtonInput.addEventListener('click', () => {
     // 테이블의 tbody 선택
     const tableBody = document.querySelector(".contianer_table tbody");
 
@@ -48,16 +54,36 @@ addButtonInput.addEventListener('click', async() => {
 
     // 각 열 생성 및 데이터 추가
     const dateCell = document.createElement("td");
-    dateCell.textContent = dateValue || "X";
+    if (!dateValue) {
+        alert("날짜를 입력해주세요.");
+        dateInput.focus();
+        return;
+    }
+    dateCell.textContent = dateValue;
 
     const optionCell = document.createElement("td");
-    optionCell.textContent = selectedOption || "X";
+    if (selectedOption === "입/출금") {
+        alert("입금 또는 출금을 선택해주세요.");
+        selectOption.focus();
+        return;
+    }
+    optionCell.textContent = selectedOption;
 
     const descriptionCell = document.createElement("td");
-    descriptionCell.textContent = descriptionValue || "X";
+    if (!descriptionValue) {
+        alert("내역을 입력해주세요.");
+        descriptionInput.focus();
+        return;
+    }
+    descriptionCell.textContent = descriptionValue;
 
     const moneyCell = document.createElement("td");
-    moneyCell.textContent = moneyValue || "X";
+    if (!moneyValue) {
+        alert("금액을 입력해주세요.");
+        moneyInput.focus();
+        return;
+    }
+    moneyCell.textContent = moneyValue;
 
     // 새로운 행에 열 추가
     newRow.appendChild(dateCell);
@@ -74,7 +100,9 @@ addButtonInput.addEventListener('click', async() => {
     descriptionValue = "";
     moneyValue = "";
 
-    console.log("새로운 행이 추가되었습니다!");
+
+    alert("데이터가 성공적으로 추가되었습니다.");
 
 })
 
+/* 추가 버튼 누를 시 필요한 입력 값들 GRAPH에 전달 */
